9422e5c9cdec245890045b2f244757c8c03157db
r4cs <r.guzansky@hotmail.com>
2024-07-04 14:11:13 -0300

    detalhes estéticos

--- src/services/Firebase.js
+++ src/services/Firebase.js
@@ -14,7 +14,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database = getDatabase(app);
